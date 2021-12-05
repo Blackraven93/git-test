95c4bddb8129443065136494d149266ba4b0f8ac
곽세진_2004_anl <Reblackraven@Gmail.com>
2021-12-06 00:19:27 +0900

    Feat create ReactDOM.js

---
+++ reactDOM.js
@@ -0,0 +1,22 @@
+const render = (creatingTagName, tagNameToPut, c) => {
+  const root = document.getElementById(tagNameToPut);
+  const tagName = document.createElement(creatingTagName);
+  root.textContent = document.createElement(creatingTagName);
+};
+
+class React {
+  constructor() {
+    this.tagName = tagName;
+  }
+
+  createElement(tagName, property) {}
+}
+
+class ReactDOM {
+  constructor() {
+    this.createdElement = createdElement;
+    this.target = target;
+  }
+
+  render(createElement, target) {}
+}
